33c498d76e7ab09e158ab4eea8e4410b1b4b011a
Patrick Allen <prallen90@gmail.com>
2017-03-04 22:39:46 -0500

    Use 'DenomedSize' class for handling str to bytes conversion

--- thyme/utils.py
+++ thyme/utils.py
@@ -1,6 +1,26 @@
 import random
 
 
+class DenomedSize(object):
+    def __init__(self, value, byterate):
+        self.byterate = byterate
+        self.value = value
+
+    @classmethod
+    def from_string(cls, string):
+        """Evaluate a size string and turn it into a DenomedSize instance."""
+        value, denom = _parse_size_string(string)
+        try:
+            return cls(value, RATE_MAP[denom.lower()])
+        except KeyError:
+            raise ValueError(
+                "'{}' is not a valid size denomination.".format(denom))
+
+    def to_bytes(self):
+        """Return the size converted into bytes."""
+        return self.byterate * self.value
+
+
 class ByteRate(object):
     """A simple helper object for encapsulating byte rate information."""
 
@@ -12,46 +32,39 @@ class ByteRate(object):
     def __mul__(self, other):
         return other * self.value
 
-
-class ByteRateEnum(object):
-    """Conversion rates for denominations from bytes."""
-
-    BYTE     = ByteRate(1, 'Bytes', 'b')
-    KILOBYTE = ByteRate(1024, 'Kilobytes', 'kb')
-    MEGABYTE = ByteRate(1048576, 'Megabytes', 'mb')
-    GIGABYTE = ByteRate(1073741824, 'Gigabytes', 'gb')
-    TERABYTE = ByteRate(1099511627776, 'Terabytes', 'tb')
-    PETABYTE = ByteRate(1125899906842624, 'Petabytes', 'pb')
-
-
 RATE_MAP = {
-    'b' : ByteRateEnum.BYTE,
-    'kb': ByteRateEnum.KILOBYTE,
-    'mb': ByteRateEnum.MEGABYTE,
-    'gb': ByteRateEnum.GIGABYTE,
-    'tb': ByteRateEnum.TERABYTE,
-    'pb': ByteRateEnum.PETABYTE,
+    'b': ByteRate(1, 'Bytes', 'b'),
+    'kb': ByteRate(1024, 'Kilobytes', 'kb'),
+    'mb': ByteRate(1048576, 'Megabytes', 'mb'),
+    'gb': ByteRate(1073741824, 'Gigabytes', 'gb'),
+    'tb': ByteRate(1099511627776, 'Terabytes', 'tb'),
+    'pb': ByteRate(1125899906842624, 'Petabytes', 'pb'),
 }
 
 
-def to_bytes(val, denom):
-    """Convert kb, mb, gb, tb, or pb, to bytes."""
-    try:
-        rate = RATE_MAP[denom].value
-    except KeyError:
-        raise ValueError("Not a valid denomination.")
-    return val * float(rate)
+def _parse_size_string(string):
+    for index, char in enumerate(str(string)):
+        if char not in '1234567890':
+            return (int(string[:index]), string[index:])
+    raise ValueError("Size string requires digits and letters.")
 
 
 def get_all_rates(bytes_):
     """Convert bytes to kb, mb, gb, tb, and pb."""
-    converteds = [(v * bytes_, k) for k, v in RATE_MAP.items()]
-    return sorted(converteds, key=lambda x: x[0])
+    converteds = [(bytes_ / v.value, k) for k, v in RATE_MAP.items()]
+    return sorted(converteds, key=lambda x: x[0], reverse=True)
 
 
 def format_rates(rates):
     """Return the rates formatted in a visually appealing way."""
-    formatted = ("{}:\t{}".format(RATE_MAP[r[1]].name, r[0]) for r in rates)
+    indent = max([len(r.name) for r in RATE_MAP.values()])
+
+    def _pad_name(name):
+        padding = " " * (indent - len(name))
+        return "{0}{1}:  ".format(padding, name)
+
+    tups = [(_pad_name(RATE_MAP[r[1]].name), r[0]) for r in rates]
+    formatted = ("{}{:g}".format(*tup) for tup in tups)
     return "\n".join(formatted)
 
 
